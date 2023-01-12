bd1665aa24865c365b26043c3ea8cad9bd91728c
Marta Liushniak <marta.liushniak@yahoo.com>
2023-01-12 23:09:18 +0200

    meeting solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;"
+                + "Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"));
+    }
 
+    public static String meeting(String s) {
+        String[] guests = s.toUpperCase().split(";");
+        for (int i = 0; i < guests.length; i++) {
+            int index = guests[i].indexOf(":");
+            guests[i] = "(" + guests[i].substring(index + 1) + ", "
+                    + guests[i].substring(0, index) + ")";
+        }
+        Arrays.sort(guests);
+        return String.join("",guests);
     }
 }
